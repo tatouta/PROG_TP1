b8016fc6ca6f1eaf65db4525637d4837235546a8
tatouta <taha.khalfi.ob@gmail.com>
2025-03-06 21:58:47 -0500

    almost CommandProcessor commit

--- src/services/MusicLibrary.java
+++ src/services/MusicLibrary.java
@@ -20,12 +20,12 @@ public class MusicLibrary {
         this.search = 0;
     }
 
-    // pour ajouter un élément de musique à la librairie.
+    // item methods
+
     public void addItem( MusicItem item ) {
         this.items.add(item);
     }
 
-    // pour retirer l'élément de musique avec l'id de la librairie.
     public void removeItem( int id ) {
         int index = findIndex(id);
         if (index > -1) {
@@ -51,17 +51,38 @@ public class MusicLibrary {
                 break;
             }
         }
+        if (index > -1) {
+            Message.send("The item with the specified ID does not exist.");
+        }
+        return index;
+    }
+
+    public int findIndex(String title, String artist) {
+        int index = -1;
+        for (int i = 0; i < this.items.size(); i++) {
+            MusicItem item = this.items.get(i);
+            if (!item.getInfo().equals("Podcast") && item.getTitle() == title) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            Message.send("The item with the specified ID does not exist.");
+        }
         return index;
     }
 
     public void searchItem(int id) {
-        MusicItem item = findItem(id);
-        if (item != null) {
+        int index = findIndex(id);
+        if (index > -1) {
             this.search = findIndex(id);
         }
     }
 
-    // pour lister les éléments de musique présents dans la librairie.
+    public void searchItem(String title, String artist) {
+
+    }
+
     public void listAllItems() {
         String display = "";
         int size = this.items.size();
@@ -76,25 +97,21 @@ public class MusicLibrary {
         Message.send(display);
     }
 
-    // pour lancer/jouer l'élément musical id.
     public void playItem( int id ) {
         MusicItem item = this.items.get(this.search);
         item.play();
     }
 
-    // pour mettre sur pause l'élément musical qui joue.
     public void pauseItem() {
         MusicItem item = this.items.get(this.search);
         item.pause();
     }
 
-    // pour arrêter l'élément musical qui joue.
     public void stopItem() {
         MusicItem item = this.items.get(this.search);
         item.stop();
     }
 
-    // pour vider la liste des éléments musicaux présents dans la librairie
     public void clearAllItems() {
         this.items.clear();
         Message.send("Music library has been cleared successfully.");
